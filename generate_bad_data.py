dae7575710c3b7e6cf8e7869ea0c6b31fea7a044
Yuyu Lai <yuyulai97@gmail.com>
2019-12-01 13:10:42 -0800

    added generate bad data script

---
+++ generate_bad_data.py
@@ -0,0 +1,35 @@
+from matplotlib import style
+
+style.use("ggplot")
+import csv
+import json
+import os
+import pandas as pd
+from sklearn.svm import SVR
+import glob
+
+
+class BadDataGenerator:
+
+    def __init__(self, file_dir):
+        print("generating bad data...")
+        self.file_dir = file_dir
+
+    def open_files(self):
+        all_files = glob.glob(self.file_dir + "/*.csv")
+        emp_list = []
+
+        for filename in all_files:
+            df = pd.read_csv(filename, index_col=0, header=0)
+            emp_list.append(df)
+            current_mean = df["Alt"].mean()
+            print("mean of Alt for ", filename, " is ", df["Alt"].mean())
+            dfupdate= df.sample(60)
+            bad_alt = current_mean * 0.90
+            dfupdate.Alt = bad_alt
+            df.update(dfupdate)
+            newfile = filename.split("\\")
+            df.to_csv("./compared_auto/" + newfile[1])
+
+generator = BadDataGenerator("./formatted_auto")
+generator.open_files()
